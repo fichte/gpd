76c3568086c52ead09c21d23dae845fd958efb2b
fichte <fichte@amk.wtf>
2024-05-31 10:45:49 +0200

    introduce asset directory for binary artifacts

--- functions/_config_files.sh
+++ functions/_config_files.sh
@@ -1,5 +1,21 @@
 function copy_config_files()
 {
+	## initialize array of available asset files
+	pushd "${SCRIPT_DIR}"/../docker/template/asset &>/dev/null
+	ASSET_FILES_TEMPLATES=(*_template)
+	ASSET_FILES_TEMPLATES_LEN=$(( "${#ASSET_FILES_TEMPLATES[@]}" - 1 ))
+	popd &>/dev/null
+
+	for t in `seq 0 "${ASSET_FILES_TEMPLATES_LEN}"`; do
+		FINAL_CONFIG_FILE=$(echo "${ASSET_FILES_TEMPLATES[$t]}" | sed -e 's/_template//g')
+		cp "${SCRIPT_DIR}"/../docker/template/asset/"${ASSET_FILES_TEMPLATES[$t]}" "${STACK_FINAL_CONFIG_DIR}"/asset/"${FINAL_CONFIG_FILE}"
+	done
+
+	## initialize array of all final asset files
+	pushd "${STACK_FINAL_CONFIG_DIR}"/asset &>/dev/null
+	ASSET_FILES=(*)
+	popd &>/dev/null
+
 	## initialize array of available compose files
 	pushd "${SCRIPT_DIR}"/../docker/template/compose &>/dev/null
 	COMPOSE_FILES_TEMPLATES=(*_template)
@@ -11,7 +27,7 @@ function copy_config_files()
 		cp "${SCRIPT_DIR}"/../docker/template/compose/"${COMPOSE_FILES_TEMPLATES[$t]}" "${STACK_FINAL_CONFIG_DIR}"/compose/"${FINAL_CONFIG_FILE}"
 	done
 
-	## initialize array of all final config files
+	## initialize array of all final compose files
 	pushd "${STACK_FINAL_CONFIG_DIR}"/compose &>/dev/null
 	COMPOSE_FILES=(*)
 	popd &>/dev/null
--- gpd.sh
+++ gpd.sh
@@ -28,6 +28,7 @@ else
 	variables_base
 	echo "[GPD][INFO] using ci variables file \"${STACK_ENVIRONMENT_VARIABLES}\""
 	echo "[GPD][INFO] using final config directory \"${STACK_FINAL_CONFIG_DIR}\""
+	echo "[GPD][INFO] using remote/local asset directory \"${STACK_ASSET_PATH}\""
 	echo "[GPD][INFO] using remote/local compose directory \"${STACK_COMPOSE_PATH}\""
 	echo "[GPD][INFO] using remote/local config directory \"${STACK_CONFIG_PATH}\""
 	echo "[GPD][INFO] using remote/local data directory \"${STACK_DATA_PATH}\""
